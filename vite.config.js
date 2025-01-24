48be04b0d3438c924a66f5ba2f982ca352c3fe14
thulasidharan96 <thulasidharankvp@gmail.com>
2025-01-24 12:45:43 +0530

    removed all

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default {
-  build: {
-    outDir: 'dist',  // Ensure the output directory is set to 'dist'
-  }
-}
