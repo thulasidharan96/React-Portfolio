50c175b55215cf2000c8c962f66acf2a9d36e41b
thulasidharan96 <thulasidharankvp@gmail.com>
2025-01-24 12:33:16 +0530

    Portfolio

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,29 @@
-// import images
-import Hero_person from "./assets/images/Hero/person.png";
+// import 
+import Hero_person from "./assets/Hero/person.png";
 
-import figma from "./assets/images/Skills/figma.png";
-import sketch from "./assets/images/Skills/sketch.png";
-import ps from "./assets/images/Skills/ps.png";
-import reactjs from "./assets/images/Skills/react.png";
-import nodejs from "./assets/images/Skills/node.png";
-import python from "./assets/images/Skills/python.png";
-import cs1 from "./assets/images/Skills/cs1.jpg";
-import java from "./assets/images/Skills/java.png";
+import ps from "./assets/Skills/ps.png";
+import reactjs from "./assets/Skills/react.png";
+import nodejs from "./assets/Skills/node.png";
+import python from "./assets/Skills/python.png";
+import cs1 from "./assets/Skills/cs1.jpg";
+import java from "./assets/Skills/java.png";
 
-import services_logo1 from "./assets/images/Services/logo1.png";
-import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
+import services_logo1 from "./assets/Services/logo1.png";
+import services_logo2 from "./assets/Services/logo2.png";
+import services_logo3 from "./assets/Services/logo3.png";
 
-import project1 from "./assets/images/Projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/Projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/Projects/img1.png";
+import project2 from "./assets/projects/img2.png";
+import project3 from "./assets/Projects/img3.png";
+import person_project from "./assets/projects/person.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
+import avatar1 from "./assets/Testimonials/avatar1.png";
+import avatar2 from "./assets/Testimonials/avatar2.png";
+import avatar3 from "./assets/Testimonials/avatar3.png";
+import avatar4 from "./assets/Testimonials/avatar4.png";
 
-import Hireme_person from "./assets/images/Hireme/person.png";
-import Hireme_person2 from "./assets/images/Hireme/person2.png";
+import Hireme_person from "./assets/Hireme/person.png";
+import Hireme_person2 from "./assets/Hireme/person2.png";
 
 // import icons from react-icons
 import { GrMail } from "react-icons/gr";
