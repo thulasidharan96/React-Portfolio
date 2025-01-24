acc24864306891959158089dedf359daaab324a8
thulasidharan96 <thulasidharankvp@gmail.com>
2025-01-24 12:27:42 +0530

    Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
+export default {
   build: {
-    rollupOptions: {
-      external: [
-        './assets/images/projects/img2.png'
-      ]
-    }
+    outDir: 'dist',  // Ensure the output directory is set to 'dist'
   }
-})
+}
